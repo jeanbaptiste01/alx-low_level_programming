8e1b488ee04dd5a3802a9b9840eff27aab2da34d
jeanbaptiste01 <muhayejean7@gmail.com>
2023-08-10 15:23:20 -0700

    printf

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+/**
+ * main - This is the main function
+ *
+ * Return: Always 0
+ */
+
+int main(void)
+{
+	puts("\"with proper grammar, but the outcome is a piece of art,\n");
+	return (0);
+}
