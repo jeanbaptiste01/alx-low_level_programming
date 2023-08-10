01af7ef9172ec7fab7ecfb1f113302a0714dfba0
jeanbaptiste01 <muhayejean7@gmail.com>
2023-08-10 15:28:44 -0700

    printf

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -3,11 +3,11 @@
 /**
  * main - This is the main function
  *
- * Return: Always 0
+ * Return: Always 0 when successfull
  */
 
 int main(void)
 {
-	puts("\"with proper grammar, but the outcome is a piece of art,\n");
+	puts("with proper grammar, but the outcome is a piece of art,\n");
 	return (0);
 }
