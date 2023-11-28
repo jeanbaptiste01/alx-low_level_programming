154f89af660122ec1c06bb5c521ec371f22769f5
jeanbaptiste01 <muhayejean7@gmail.com>
2023-11-27 16:40:22 -0800

    100-elf_header

--- 0x15-file_io/100-elf_header.c
+++ 0x15-file_io/100-elf_header.c
@@ -25,34 +25,35 @@ void display_elf_header(const Elf32_Ehdr *header);
 
 int main(int argc, char *argv[]) {
     if (argc != 2) {
-        fprintf(stderr, "Usage: %s elf_filename\n", argv[0]);
+        fprintf(stderr, "Usage: %s filename\n", argv[0]);
         exit(98);
     }
 
-    const char *elf_filename = argv[1];
+    const char *filename = argv[1];
 
-    int elf_file = open(elf_filename, O_RDONLY);
-    if (elf_file == -1) {
+    int file = open(filename, O_RDONLY);
+    if (file == -1) {
         perror("Error opening file");
         exit(98);
     }
 
-    Elf32_Ehdr elf_header;
-    if (read(elf_file, &elf_header, sizeof(Elf32_Ehdr)) != sizeof(Elf32_Ehdr)) {
+    Elf32_Ehdr header;
+    if (read(file, &header, sizeof(Elf32_Ehdr)) != sizeof(Elf32_Ehdr)) {
         perror("Error reading ELF header");
-        close(elf_file);
+        close(file);
         exit(98);
     }
 
-    if (elf_header.e_ident[1] != 'E' || elf_header.e_ident[2] != 'L' || elf_header.e_ident[3] != 'F') {
+    if (header.e_ident[1] != 'E' || header.e_ident[2] != 'L' || header.e_ident[3] != 'F') {
         fprintf(stderr, "Error: Not an ELF file\n");
-        close(elf_file);
+        close(file);
         exit(98);
     }
 
-    display_elf_header(&elf_header);
+    printf("ELF Header:\n");
+    display_elf_header(&header);
 
-    close(elf_file);
+    close(file);
 
     return 0;
 }
@@ -62,18 +63,18 @@ int main(int argc, char *argv[]) {
  * @header: Pointer to the ELF header structure.
  */
 void display_elf_header(const Elf32_Ehdr *header) {
-    printf("Magic:   ");
+    printf("  Magic:   ");
     for (int i = 0; i < 16; ++i)
         printf("%02x ", header->e_ident[i]);
     printf("\n");
 
-    printf("Class:                             ELF%d\n", header->e_ident[4] == 1 ? 32 : 64);
-    printf("Data:                              %s\n", header->e_ident[5] == 1 ? "2's complement, little endian" : "2's complement, big endian");
-    printf("Version:                           %d (current)\n", header->e_version);
-    printf("OS/ABI:                            UNIX - System V\n");
-    printf("ABI Version:                       0\n");
-    printf("Type:                              %s\n", header->e_type == 2 ? "EXEC (Executable file)" : "Not specified");
-    printf("Entry point address:               0x%x\n", header->e_entry);
+    printf("  Class:                             ELF%d\n", header->e_ident[4] == 1 ? 32 : 64);
+    printf("  Data:                              %s\n", header->e_ident[5] == 1 ? "2's complement, little endian" : "2's complement, big endian");
+    printf("  Version:                           %d (current)\n", header->e_version);
+    printf("  OS/ABI:                            UNIX - System V\n");
+    printf("  ABI Version:                       0\n");
+    printf("  Type:                              %s\n", header->e_type == 2 ? "EXEC (Executable file)" : "Not specified");
+    printf("  Entry point address:               0x%x\n", header->e_entry);
 
     printf("\n");
 }
